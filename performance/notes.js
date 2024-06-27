6472a99e7dcede8c4fe4a1b115f9254c136c1c07
Shreyansh Goyal <shreyansh.goyal@crimsoni.com>
2024-06-27 12:03:37 +0530

    add all the code changes

---
+++ performance/notes.js
@@ -0,0 +1,40 @@
+// Client side rendering vs Server side rendering
+
+// Client Side Rendering        
+// Pros                             Cons
+// Rich Interaction                 Slower Initial Load
+// Reduced Server Interactions      Bad For Search Engine Optimization
+// Better Client Side Caching       Dependecy on the user device
+
+
+// Server Side Rendering
+// Pros                     Cons
+// Faster Initial Load      Increase Load on Server
+// Better SEO               Frequent Server Request can increase latency
+// Consistent Performance   More Complex to implement
+
+
+
+
+// Core Web Vitals
+// LCP : Largest Contentful Paint : Largest Section visible to user when user loads the route
+// How to improve it : by converting the images from the png to webp
+// LCP is measured on the mobile first therefore we need to improve it over there first then 
+// we can improve it elsewhere
+
+// CLS : Cumulative layout shift
+// CLS is measure of largest burst of layout shift score for every unexpected layout shift that occurs
+// during the lifespan of page
+
+
+// INP : Interaction to next paint
+// If there is a blocking task and that blocking task is creating too much rampage such that
+// next paint is taking time then our score will be bad
+
+
+
+// Accessiblity
+// Web accessiblity means that websites, tools, and technologies are designed and developed so 
+// that people with disabilities can use them
+
+
